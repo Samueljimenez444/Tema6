f008da21e94d79d8fcb2713a73708e9c78b4bb17
Samueljimenez444 <samuel.jimenez@iesnervion.es>
2025-04-22 14:41:26 +0200

    Ejercicios del 1 al 4

--- Tema 6/src/ejercicio4Parte1/Ejercicio4.java
+++ Tema 6/src/ejercicio4Parte1/Ejercicio4.java
@@ -3,12 +3,18 @@ package ejercicio4Parte1;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Ejercicio4 {
 
 	public static void main(String[] args) {
+		String texto="";
+		Scanner lectura=new Scanner(System.in);
 		try(BufferedWriter bw= new BufferedWriter(new FileWriter("src\\ejercicio4Parte1\\Texto.txt"))){
-			
+			while(!texto.equals("salir"))
+			System.out.println("Introduce el texto para el fichero");
+			texto=lectura.nextLine();
+			bw.write(texto);
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 		}
